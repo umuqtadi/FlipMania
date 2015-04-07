68c42210779fb1150a12c54ad0444e1de0372cc1
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-07 17:57:48 -0600

    Initial Commit

---
+++ FlipAMania/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipAMania
+{
+    class Program
+    {
+        static Random rng = new Random();
+        static int numberOfHeads = 0;
+        static int numberOfTails = 0;
+        static void Main(string[] args)
+        {
+        }
+        public static string numberOfFlips()
+        {
+            if (rng.Next(2) == 0)
+            {
+                numberOfHeads++;
+                return "Heads";
+            }
+            else 
+            {
+                numberOfTails++;
+                return "Tails";
+            }
+    }
+}
