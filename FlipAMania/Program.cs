1ab5d6f434065a46d9349524273f3c66ab6d0dc0
Umar Muqtadir <umar.muqtadir@gmail.com>
2015-04-08 13:03:27 -0600

    Complete

--- FlipAMania/Program.cs
+++ FlipAMania/Program.cs
@@ -8,23 +8,57 @@ namespace FlipAMania
 {
     class Program
     {
-        static Random rng = new Random();
-        static int numberOfHeads = 0;
-        static int numberOfTails = 0;
         static void Main(string[] args)
         {
+            FlipForHeads(10000);
+            flipCoins(10000);
+            Console.ReadKey();
         }
-        public static string numberOfFlips()
+        public static void flipCoins(int numberOfFlips)
         {
-            if (rng.Next(2) == 0)
+            Random rng = new Random();
+            int numberOfHeads = 0;
+            int numberOfTails = 0;
+
+            for (int i = 0; i < numberOfFlips; i++)
             {
-                numberOfHeads++;
-                return "Heads";
+
+
+                if (rng.Next(2) == 0)
+                {
+                    numberOfHeads++;
+                }
+                else
+                {
+                    numberOfTails++;
+                }
             }
-            else 
+            Console.WriteLine("We flipped a coin " + numberOfFlips + " times.");
+            Console.WriteLine("Number of heads: " + numberOfHeads);
+            Console.WriteLine("Number of tails: " + numberOfTails);
+        }
+
+        static void FlipForHeads(int numberOfHeads)
+        {
+            Random rng = new Random();
+            int numberOfHeadsFlipped = 0;
+            int totalFlips = 0;
+
+            while (numberOfHeadsFlipped != numberOfHeads)
             {
-                numberOfTails++;
-                return "Tails";
+                if (rng.Next(2) == 0)
+                {
+                    totalFlips++;
+                    numberOfHeadsFlipped++;
+                }
+                else
+                {
+                    totalFlips++;
+                }
             }
+            Console.WriteLine("We are flipping a coin until we find " + numberOfHeads + "heads");
+            Console.WriteLine("It took " + totalFlips + " to find " + numberOfHeads + " heads");
+        }
+
     }
 }
